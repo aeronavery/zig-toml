41b7ef6aec81fbfdbf31e0d3f2afbbe987f908c9
Robert Burnett <dr.slate2@gmail.com>
2024-01-21 11:39:28 -0600

    updated build.zig to comply with zig master

--- build.zig
+++ build.zig
@@ -1,7 +1,6 @@
-const Builder = @import("std").build.Builder;
 const std = @import("std");
 
-pub fn build(b: *Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const lib = b.addStaticLibrary(.{
@@ -11,10 +10,10 @@ pub fn build(b: *Builder) void {
         .target = target,
     });
     b.installArtifact(lib);
-    const module = b.addModule("toml", std.Build.CreateModuleOptions{
-        .source_file = .{ .path = "src/toml.zig" },
+    const module = b.addModule("toml", .{
+        .root_source_file = .{ .path = "src/toml.zig" },
     });
-    lib.addModule("toml", module);
+    lib.root_module.addImport("toml", module);
 
     const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/toml.zig" },
